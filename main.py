0e1012bab336d3cece2da9fa833c93936ad7d7f5
25na25 <137747783+25na25@users.noreply.github.com>
2026-05-11 01:45:40 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,21 @@ from discord.ext import commands
 from discord import app_commands
 from discord.ui import View, Select, Button
 
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "alive"
+
+def run():
+    app.run(host="0.0.0.0", port=10000)
+
+threading.Thread(target=run).start()
+
+
 TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
